a2d0cc69e60ac3301451bd694be8772fe332c71a
franose371 <franose371@163.com>
2017-05-31 15:12:22 +0800

    modify showPic() and add some stylesheets

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,5 +1,10 @@
 function showPic(whichPic){
+	//改变图片显示
 	var source = whichPic.getAttribute("href");
 	var placeholder = document.getElementById("placeholder");
 	placeholder.setAttribute("src",source);
+	//改变文字显示
+	var text = whichPic.getAttribute("title");
+	var description = document.getElementById("description");
+	description.firstChild.nodeValue = text;
 }
