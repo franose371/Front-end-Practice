467414a12245937b3c52cc9d362bb5a275316be7
franose371 <franose371@163.com>
2017-07-09 09:12:50 +0800

    update datepicker

--- datepicker/date.js
+++ datepicker/date.js
@@ -1,26 +1,64 @@
-(function () {
+(function() {
 	var datepicker = {};
-	datepicker.getMOnthData = funciton(year,month) {
+	datepicker.getMonthData = function(year, month){
 		var ret = [];
-		if (!year || !month) {
+		//若没有参数就使用当前日期
+		if ((!year || !month) && month != 0) {
 			var today = new Date();
+			year = today.getFullYear();
 			month = today.getMonth() + 1;
 		}
-		var firstDay = new Date(year,month - 1, 1);
+
+		//传入数据时month需要-1，获得传入参数月份的第一天
+		var firstDay = new Date(year, month - 1, 1);
+
+		year = firstDay.getFullYear();
+		month = firstDay.getMonth() + 1;
+		//当月第一天是星期几
 		var fisrtDayWeekDay = firstDay.getDay();
 		if (fisrtDayWeekDay === 0) fisrtDayWeekDay = 7;
-
+		//上月最后一天，获得的是当月的第0天
 		var lastDayOfLastMonth = new Date(year, month - 1, 0);
+		//上月的最后一天是上月的第几天，即上月有多少天
 		var lastDateOfLastMonth = lastDayOfLastMonth.getDate();
-
+		//当月日历需要在第一行显示上月的几天，日历排列为一二三四五六日，若当月第一天为周一，则需要显示0天，周二则需要显示1天
 		var preMonthDayCount = fisrtDayWeekDay - 1;
 
+		//本月最后一天，获得的是下个月的第0天，越界定位
 		var lastDay = new Date(year, month, 0);
+		//是本月的第几天，即本月有多少天
 		var lastDate = lastDay.getDate();
 
-		for (var i = 0; i < 7*6; i++) {
-			var date = i -
+		for (var i = 0; i < 7 * 6; i++) {
+			//日历显示的第i个数是当月哪一天
+			var date = i + 1 - preMonthDayCount;
+			var showDate = date;
+			var thisMonth = month;
+
+			if (date <= 0) {
+				//上一月
+				thisMonth = month - 1;
+				showDate = lastDateOfLastMonth + date;
+			} else if (date > lastDate) {
+				//下一月
+				thisMonth = month + 1;
+				showDate = showDate - lastDate;
+			}
+
+			if (month === 0) thisMonth = 12;
+			if (month === 13) thisMonth = 1;
+
+			ret.push({
+				month: thisMonth,
+				date: date,
+				showDate: showDate
+			});
 		}
-	}
+		return {
+			year: year,
+			month: month,
+			days: ret
+		};
+	};
 	window.datepicker = datepicker;
 })();
---
+++ datepicker/main.js
@@ -0,0 +1,114 @@
+(function() {
+	var datepicker = window.datepicker;
+	//数据渲染函数
+	var monthData;
+	var $wrapper;
+	datepicker.buildUi = function(year, month) {
+		monthData = datepicker.getMonthData(year, month);
+		var html = '		<div class="ui-datepicker-header">' +
+			'<a href="#" class="ui-datepicker-btn ui-datepicker-prev-btn">&lt;</a>' +
+			'<a href="#" class="ui-datepicker-btn ui-datepicker-next-btn">&gt;</a>' +
+			'<span class="ui-datepicker-curr-month">' + monthData.year + '-' + monthData.month + '</span>' +
+			'</div>' +
+			'<div class="ui-datepicker-body">' +
+			'<table>' +
+			'<thead>' +
+			'	<tr>' +
+			'<th>一</th>' +
+			'<th>二</th>' +
+			'<th>三</th>' +
+			'<th>四</th>' +
+			'						<th>五</th>' +
+			'						<th>六</th>' +
+			'						<th>日</th>' +
+			'					</tr>' +
+			'				</thead>' +
+			'				<tbody>';
+
+		for (var i = 0; i < monthData.days.length; i++) {
+			var data = monthData.days[i];
+			//i能被7整除，即每周第一天
+			if (i % 7 === 0) {
+				html += '<tr>';
+			}
+
+			html += '<td>' + data.showDate + '</td>'
+				//余6，为每周的最后一天
+			if (i % 7 === 6) {
+				html += '</tr>';
+			}
+		}
+		html += '				</tbody>' +
+			'			</table>' +
+			'</div>';
+
+		return html;
+	};
+
+	datepicker.render = function(direction) {
+		var year, month;
+		if (monthData) {
+			year = monthData.year;
+			month = monthData.month;
+		}
+
+		if (direction === 'prev') {
+			month = month - 1;
+		}
+
+		if (direction === 'next') {
+			month = month + 1;
+		}
+
+		var html = datepicker.buildUi(year, month);
+		wrapper = document.querySelector('.ui-datepicker-wrapper');
+		if (!wrapper) {
+			$wrapper = document.createElement('div');
+			document.body.appendChild($wrapper);
+			$wrapper.className = "ui-datepicker-wrapper";
+		}
+		$wrapper.innerHTML = html;
+
+	};
+
+	datepicker.init = function(input) {
+		datepicker.render();
+
+		var $input = input;
+		var isOpen = false;
+
+		$input.addEventListener('click', function() {
+			if (isOpen) {
+				$wrapper.classList.remove('ui-datepicker-wrapper-show');
+				isOpen = false;
+			} else {
+				$wrapper.classList.add('ui-datepicker-wrapper-show');
+				var left = $input.offsetLeft;
+				var top = $input.offsetTop;
+				var height = $input.offsetHeight;
+				$wrapper.style.top = top + height + 2 + 'px';
+				$wrapper.style.left = left;
+				isOpen = true;
+			}
+		}, false);
+
+		// $input.addEventListener('focusout', function() {
+		// 	if (isOpen) {
+		// 		$wrapper.classList.remove('ui-datepicker-wrapper-show');
+		// 		isOpen = false;
+		// 	}
+		// }, false);
+
+		$wrapper.addEventListener('click', function(e) {
+			var $target = e.target;
+			//上一月
+			if ($target.classList.contains('ui-datepicker-prev-btn')) {
+				datepicker.render('prev');
+
+			} else if ($target.classList.contains('ui-datepicker-next-btn')) {
+				datepicker.render('next');
+
+			}
+		}, false);
+	};
+})();
